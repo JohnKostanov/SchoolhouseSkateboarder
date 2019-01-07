7cbb9a36c7094261d4dfa54982694c5b11885f08
Andrian Kryk <andriancrick@gmail.com>
2019-01-07 15:49:33 +0200

    page 269

---
+++ SchoolhouseSkateboarder/Skater.swift
@@ -0,0 +1,19 @@
+//
+//  Skater.swift
+//  SchoolhouseSkateboarder
+//
+//  Created by Andrian Kryk on 1/6/19.
+//  Copyright © 2019 Andrian Kryk. All rights reserved.
+//
+
+import SpriteKit
+
+class Skater: SKSpriteNode {
+
+	var velocity = CGPoint.zero
+	var minimumY: CGFloat = 0.0
+	var jumpSpead: CGFloat = 20.0
+	var isOnGround = true
+	
+}
+
